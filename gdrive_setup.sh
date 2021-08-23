1c24958d15b210a496da1609f9edc7b4f177ac6d
Ian Culp <57159226+icculp@users.noreply.github.com>
2021-08-23 12:24:51 -0500

    Update gdrive_setup.sh

--- gdrive_setup.sh
+++ gdrive_setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-wget -O gdrive https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
-chmod +x gdrive
-sudo install gdrive /usr/local/bin/gdrive
+wget -O gdrive 'https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download'
+chmod +x ./gdrive
+sudo install ./gdrive /usr/local/bin/gdrive
 gdrive list
-# paste output on screen to a browser where your google account is logged in, and give access to project
+# paste output on screen to a browser where your google account is logged in, and give access to project, then copy the value given to give back to the prompt
+# Now you can do:
+# gdrive upload /dir/filename.tar.gz
