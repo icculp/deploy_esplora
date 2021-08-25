3c8867d5db88eb32a84ffdff39dda40b0199bdd2
Ian Culp <icculp@gmail.com>
2021-08-25 17:45:49 +0000

    gdrive downloading

--- gdrive_download.sh
+++ gdrive_download.sh
@@ -2,11 +2,20 @@
 # put googleID of filename here
 # when you share the file, take the id from link like https://drive.google.com/file/d/1O4jtsCP36RdEOkLxn1f1MY0hqa1F6DLy/view?usp=sharing
 # ggID=1O4jtsCP36RdEOkLxn1f1MY0hqa1F6DLy
-ggID='1O4jtsCP36RdEOkLxn1f1MY0hqa1F6DLy'  
-ggURL='https://drive.google.com/uc?export=download'  
-filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"  
-getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"  
-curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o lightmode.tar.gz
+
+# run gdrive setup and link to google account with tar
+# `gdrive list` to get id
+# gdrive download <id>
+
+
+# Had trouble with this method
+
+#ggID='1O4jtsCP36RdEOkLxn1f1MY0hqa1F6DLy'  
+#ggURL='https://drive.google.com/uc?export=download'  
+#filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"  
+#getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"  
+#curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o lightmode.tar.gz
+
 # add below if you want to specify filename var
 # -o "${filename}"  
 
