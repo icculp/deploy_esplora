983c19e542a821742fc675492feaa0b928bf4714
Ian Culp <57159226+icculp@users.noreply.github.com>
2021-08-26 21:49:59 -0500

    Update unzip_inplace.sh

--- unzip_inplace.sh
+++ unzip_inplace.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+'''
+If you don't have enough space left on the drive to hold the compressed and decompressed archives, you can decompress it 'in-place' by extracing a single file at a time and subsequently deleting it, but this requires updating the entire archive (rewriting it to disk) every time which can be very time consuming. 
+'''
+
 filename=lightmode.tar.gz
 
 # tar --delete --file=googlecl-0.9.7.tar googlecl-0.9.7/README.txt
