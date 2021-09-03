f61b3e0f14dd45bed794c759bcc136ad5aa13e83
Ian Culp <icculp@gmail.com>
2021-09-03 23:58:59 +0000

    adding onion-location to header

--- tor.sh
+++ tor.sh
@@ -13,6 +13,10 @@ sudo /etc/init.d/tor restart
 onion_v3=$(sudo cat /var/lib/tor/mainnet/hostname)
 sudo sed -i "s!export ONION_V3=.*!export ONION_V3=\"http://$onion_v3:8080\"!" /mnt/disks/sdb/esplora/flavors/bitcoin-mainnet/config.env
 
+cat >> /mnt/disks/sdb/esplora/flavors/bitcoin-mainnet/config.env << EOF
+export HEAD_HTML="<meta http-equiv="onion-location" content="http://$onion_v3:8080" />"
+EOF
+
 cd /mnt/disks/sdb/esplora
 sudo systemctl stop mainnet
 sudo docker build -t esplora .
